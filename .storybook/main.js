57787ddcad056f82ad9cde629aa2b9a333729c12
Katie Nolan <nolakat@gmail.com>
2022-01-25 19:23:52 -0500

    Fix postcss

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,24 +11,5 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
   ],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: [
-        {
-          loader: 'postcss-loader',
-          options: {
-            ident: 'postcss',
-            plugins: [
-              require('tailwindcss'),
-              require('autoprefixer'),
-            ],
-          },
-        },
-      ],
-      include: path.resolve(__dirname, '../'),
-    })
-    return config
-  },
   "framework": "@storybook/vue"
 }
