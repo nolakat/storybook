c1a1d7a0003672d42a1cc3baf21c1165d78678d4
Katie Nolan <nolakat@gmail.com>
2022-02-27 19:34:02 -0500

    New Build

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,7 +10,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "storybook-addon-designs"
+    "storybook-addon-designs",
+    '@storybook/addon-a11y'
   ],
   "framework": "@storybook/vue",
 }
